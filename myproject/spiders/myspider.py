79cc412b8f6bce460bbe92f1b68c7f297b18b922
Ayorinde Olanipekun, PhD <51823754+AyorindeTayo@users.noreply.github.com>
2023-04-04 12:57:02 +0200

    Add files via upload

---
+++ myproject/spiders/myspider.py
@@ -0,0 +1,28 @@
+
+import scrapy
+from scrapy.spiders import CrawlSpider, Rule
+from scrapy.linkextractors import LinkExtractor
+
+
+class Myspider(CrawlSpider):
+    name = "myspider"
+    allowed_domains = ["toscrape.com"]
+    start_urls = ["http://books.toscrape.com/"]
+
+
+    rules =(
+        Rule(LinkExtractor(allow="catalogue/category")),
+        Rule(LinkExtractor(allow="catalogue", deny ="category"), callback="parse_item"),
+
+
+    )
+
+    def parse_item(self, response):
+        yield {
+            "title": response.css(".product_main h1::text").get(),
+            "price": response.css(".price_colour::text").get(),
+            "availability": response.css(".availability::text")[1].get()
+
+        }
+
+   
